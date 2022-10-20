d617e9a18784aa8704c340e52a53c223e9fe104d
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-10-20 11:10:30 -0600

    awd

--- main.js
+++ main.js
@@ -11,19 +11,26 @@ let cloudImg = document.getElementById("cloud");
 
 let cloud1x = 130;
 let cloud1y = 120;
+let cloud2x = 170;
+let cloud2y = 100;
 
 // Animation Loop
-requestAnimationFrame(animate);
+requestAnimationFrame(awd);
 
-function animate() {
+function awd() {
     // UPDATE
     // Animate cloud 1
-    cloud1x--; // Move left
+    cloud1x++; // Move Right
+    cloud2x += 2;
 
-    // If cloud goes off left side of canvas, teleport to right side at random height
-    if (cloud1x + 100 < 0) {
-        cloud1x = cnv.width;
+    // If cloud goes off Right side of canvas, teleport to left side at random height
+    if (cloud1x - 100 > 300) {
+        cloud1x = -75;
         cloud1y = Math.random() * 200;
+    }
+    if (cloud2x - 100 > 300) {
+        cloud2x = -75;
+        cloud2y = Math.random() * 200;
     }
 
 
@@ -38,9 +45,101 @@ function animate() {
 
     // Draw Clouds
     ctx.drawImage(cloudImg, cloud1x, cloud1y); // Cloud 1
-    ctx.drawImage(cloudImg, 170, 100); // Cloud 2
+    ctx.drawImage(cloudImg, cloud2x, cloud2y); // Cloud 2
     
 
     // LOOP
+    requestAnimationFrame(awd);
+}
+
+document.getElementById("btn1").addEventListener("click", norm);
+function norm() {
+
+    let cloud1x = 130;
+    let cloud1y = 120;
+    let cloud2x = 170;
+    let cloud2y = 100;
+    // Animation Loop
     requestAnimationFrame(animate);
+
+    function animate() {
+        // UPDATE
+        // Animate cloud 1
+        cloud1x++; // Move Right
+        cloud2x += 2;
+
+        // If cloud goes off Right side of canvas, teleport to left side at random height
+        if (cloud1x - 100 > 300) {
+            cloud1x = -75;
+            cloud1y = Math.random() * 200;
+        }
+        if (cloud2x - 100 > 300) {
+            cloud2x = -75;
+            cloud2y = Math.random() * 200;
+        }
+
+
+        // DRAW
+        // Blue Background
+        ctx.fillStyle = "blue";
+        ctx.fillRect(0, 0, cnv.width, cnv.height);
+
+        // Green Grass
+        ctx.fillStyle = "green";
+        ctx.fillRect(0, 300, 400, 100);
+
+        // Draw Clouds
+        ctx.drawImage(cloudImg, cloud1x, cloud1y); // Cloud 1
+        ctx.drawImage(cloudImg, cloud2x, cloud2y); // Cloud 2
+        
+
+        // LOOP
+        requestAnimationFrame(animate);
+    }
+}
+
+document.getElementById("btn2").addEventListener("click", crazy);
+function crazy() {
+
+    let cloud1x = 130;
+    let cloud1y = 120;
+    let cloud2x = 170;
+    let cloud2y = 100;
+    // Animation Loop
+    requestAnimationFrame(animate2);
+
+    function animate2() {
+        // UPDATE
+        // Animate cloud 1
+        cloud1x++; // Move Right
+        cloud2x += 100;
+
+        // If cloud goes off Right side of canvas, teleport to left side at random height
+        if (cloud1x - 100 > 300) {
+            cloud1x = -75;
+            cloud1y = Math.random() * 200;
+        }
+        if (cloud2x - 100 > 300) {
+            cloud2x = -75;
+            cloud2y = Math.random() * 200;
+        }
+
+
+        // DRAW
+        // Blue Background
+        ctx.fillStyle = "blue";
+        ctx.fillRect(0, 0, cnv.width, cnv.height);
+
+        // Green Grass
+        ctx.fillStyle = "green";
+        ctx.fillRect(0, 300, 400, 100);
+
+        // Draw Clouds
+        ctx.drawImage(cloudImg, cloud1x, cloud1y); // Cloud 1
+        ctx.drawImage(cloudImg, cloud2x, cloud2y); // Cloud 2
+        
+
+        // LOOP
+        requestAnimationFrame(animate2);
+    }
 }
